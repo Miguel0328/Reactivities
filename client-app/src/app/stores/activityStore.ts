212f340527fa8a211c1be9a58a87fe85c40152c9
Miguel Farias <mfarias@axsistec.com>
2020-09-17 01:06:31 -0500

    End of section 9

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -12,11 +12,23 @@ export class ActivityStore {
   @observable submitting = false;
   @observable target = "";
 
-  @computed get activitiesByDate(): IActivity[] {
-    return Array.from(this.activityRegistry.values()).sort(
-      (a, b) => Date.parse(a.date) - Date.parse(b.date)
+  @computed get activitiesByDate(){
+    return this.groupActivitiesByDate(
+      Array.from(this.activityRegistry.values())
     );
   }
+
+  groupActivitiesByDate = (activities: IActivity[]) => {
+    const sortedActivities = activities.sort(
+      (a, b) => Date.parse(a.date) - Date.parse(b.date)
+    );
+
+    return Object.entries(sortedActivities.reduce((activities, activity) => {
+      const date = activity.date.split('T')[0];
+      activities[date] = activities[date] ? [...activities[date], activity] : [activity];
+      return activities;
+    }, {} as {[key: string]: IActivity[]}));
+  };
 
   @action loadActivities = async () => {
     this.loadingInitial = true;
@@ -28,6 +40,7 @@ export class ActivityStore {
           this.activityRegistry.set(a.id, a);
         });
       });
+      console.log(this.groupActivitiesByDate(activities));
     } catch (error) {
       console.log(error);
     } finally {
@@ -59,7 +72,7 @@ export class ActivityStore {
 
   @action clearActivity = () => {
     this.activity = undefined;
-  }
+  };
 
   getActivity = (id: string): IActivity | undefined => {
     return this.activityRegistry.get(id);
--- client-app/src/features/activities/form/ActivityForm.tsx
+++ client-app/src/features/activities/form/ActivityForm.tsx
@@ -1,5 +1,5 @@
 import React, { useState, FormEvent, useContext, useEffect } from "react";
-import { Segment, Form, Button } from "semantic-ui-react";
+import { Segment, Form, Button, Grid } from "semantic-ui-react";
 import { IActivity } from "../../../app/models/activity";
 import { v4 as uuid } from "uuid";
 import { observer } from "mobx-react-lite";
@@ -71,7 +71,9 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
   };
 
   return (
-    <Segment clearing>
+    <Grid>
+      <Grid.Column width={10}>
+      <Segment clearing>
       <Form onSubmit={handleSubmit}>
         <Form.Input
           onChange={handleInputChange}
@@ -126,6 +128,8 @@ const ActivityForm: React.FC<RouteComponentProps<DetailParams>> = ({
         />
       </Form>
     </Segment>
+      </Grid.Column>
+    </Grid>
   );
 };
 
